c7282e00eed315c61eaf5b2fdf7a512e2f382484
SreethuSNM <Sreethu@seattlenewmedia.com>
2025-05-13 03:38:22 +0530

    Update searchscript.js

--- searchscript.js
+++ searchscript.js
@@ -1,5 +1,10 @@
 console.log("Helloo");
 
+const collections = JSON.parse(document.body.getAttribute('data-selected-collections') || '[]');
+const fields = JSON.parse(document.body.getAttribute('data-selected-fields') || '[]');
+console.log(collections);
+console.log(fields);
+
 // Function to generate or get visitor ID
 async function getOrCreateVisitorId() {
     let visitorId = localStorage.getItem('visitorId');
